73e10921a9a32b079308d0360e02e493cf08d3e4
Vadym Bondarenko <vadym.bondarenko@signanthealth.com>
2023-08-17 21:30:41 +0200

    Add mongoose schema

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,24 @@
 const http = require('http')
+const mongoose = require('mongoose')
 
 const app = require('./app')
-const {loadPlanetsData} = require("./models/planets.model")
+const { loadPlanetsData } = require("./models/planets.model")
 
 const PORT = process.env.PORT || '8000'
+const MONGO_URL = 'mongodb+srv://bondvv8:oNRDoXEN1yqutdH4@nasa-project.esatold.mongodb.net/nasa?retryWrites=true&w=majority'
 
 const server = http.createServer(app)
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDb connection ready')
+})
+
+mongoose.connection.on('error', err => {
+    console.error(err)
+})
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL)
     await loadPlanetsData()
 
     server.listen(PORT, () => {
