5bf98bea8e7582cfe9ccec70f9908f707dd7819a
Vadym Bondarenko <vadym.bondarenko@signanthealth.com>
2023-08-27 20:11:38 +0200

    add .env support

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,24 +1,90 @@
+const axios = require('axios')
+
 const launchesDatabase = require('./launches.mongo')
 const planets = require('./planets.mongo')
 
 const DEFAULT_FLIGHT_NUMBER = 100
 
-const launch = {
-    flightNumber: 100,
-    mission: 'Kepler Exploration X',
-    rocket: 'Explorer IS1',
-    launchDate: new Date('December 27, 2030'),
-    target: 'Kepler-442 b',
-    customers: [
-        'ZTM',
-        'NASA',
-    ],
-    upcoming: true,
-    success: true,
+// const launch = {
+//     flightNumber: 100,
+//     mission: 'Kepler Exploration X',
+//     rocket: 'Explorer IS1',
+//     launchDate: new Date('December 27, 2030'),
+//     target: 'Kepler-442 b',
+//     customers: [
+//         'ZTM',
+//         'NASA',
+//     ],
+//     upcoming: true,
+//     success: true,
+// }
+
+const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query'
+
+async function populateLaunches() {
+    const response = await axios.post(SPACEX_API_URL, {
+        query: {},
+        options: {
+            pagination: false,
+            populate: [
+                {
+                    path: 'rocket',
+                    select: {
+                        name: 1
+                    }
+                },
+                {
+                    path: 'payloads',
+                    select: {
+                        customers: 1
+                    }
+                }
+            ]
+        }
+    })
+
+    if (response.status !== 200) {
+        console.log('Problem downloading launch data')
+        throw new Error('Launch data download failed')
+    }
+
+    const launchDocs = response.data.docs
+    for(const launchDoc of launchDocs) {
+        const payloads = launchDoc['payloads']
+        const customers = payloads.flatMap(payload => payload['customers'])
+
+        const launch = {
+            flightNumber: launchDoc['flight_number'],
+            mission: launchDoc['name'],
+            rocket: launchDoc['rocket']['name'],
+            launchDate: launchDoc['date_local'],
+            upcoming: launchDoc['upcoming'],
+            success: launchDoc['success'],
+            customers,
+        }
+
+        console.log(`${launch.flightNumber} ${launch.mission}`)
+
+        await saveLaunch(launch)
+    }
+}
+
+async function loadLaunchData() {
+    const firstLaunch = await findLaunch({flightNumber: 1, rocket: 'Falcon 1', mission: 'FalconSat'})
+    console.log(firstLaunch)
+    if (firstLaunch) {
+        console.log('Launch data already loaded')
+    } else {
+        await populateLaunches()
+    }
+}
+
+async function findLaunch(filter) {
+    return launchesDatabase.findOne(filter)
 }
 
 async function existsLaunchWithId(launchId) {
-    return launchesDatabase.findOne({flightNumber: launchId})
+    return findLaunch({flightNumber: launchId})
 }
 
 async function getLatestFlightNumber() {
@@ -33,18 +99,15 @@ async function getLatestFlightNumber() {
     return latestLaunch.flightNumber
 }
 
-async function getAllLaunches() {
+async function getAllLaunches(skip, limit) {
     return launchesDatabase
         .find({}, {_id: 0, __v: 0})
+        .sort({flightNumber: 1 })
+        .skip(skip)
+        .limit(limit)
 }
 
 async function saveLaunch(launch) {
-    const planet = await planets.findOne({keplerName: launch.target})
-
-    if (!planet) {
-        throw new Error('no matching planet found')
-    }
-
     await launchesDatabase.findOneAndUpdate({
         flightNumber: launch.flightNumber
     }, launch, {
@@ -53,6 +116,12 @@ async function saveLaunch(launch) {
 }
 
 async function scheduleNewLaunch(launch) {
+    const planet = await planets.findOne({keplerName: launch.target})
+
+    if (!planet) {
+        throw new Error('no matching planet found')
+    }
+
     const newFlightNumber = await getLatestFlightNumber() + 1
 
     const newLaunch = Object.assign(launch, {
@@ -81,4 +150,5 @@ module.exports = {
     scheduleNewLaunch,
     existsLaunchWithId,
     abortLaunchById,
+    loadLaunchData,
 }
--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-const MONGO_URL = 'mongodb+srv://bondvv8:oNRDoXEN1yqutdH4@nasa-project.esatold.mongodb.net/nasa?retryWrites=true&w=majority'
+const MONGO_URL = process.env.MONGO_URL
 
 mongoose.connection.once('open', () => {
     console.log('MongoDb connection ready')
